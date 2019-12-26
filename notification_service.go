d22afed66e0eb53254da3d99e85280fa18237183
Jinwoo Shin <bluesky0255@gmail.com>
2019-12-26 13:50:42 +0900

    Update logging not to use log.Fatal

--- notification_service.go
+++ notification_service.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+import "log"
+
 // NotificationService provides make api call with given message and shop/customer id
 type NotificationService struct {
 	TokenRepository Repository
@@ -34,34 +36,45 @@ func (e *CustomerTokenNotFoundError) Error() string {
 	return fmt.Sprintf("No token is found with customer id '%s'", e.cause)
 }
 
+// NotificationError represents internal errors in notification service
+type NotificationError struct {
+	cause interface{}
+}
+
+func (e *NotificationError) Error() string {
+	return fmt.Sprintf("Error occurred in notification service: %v", e.cause)
+}
+
 func (s *NotificationService) sendShop(shopID string, message string) error {
 	token, err := s.TokenRepository.GetShopToken(shopID)
+	defer func() {
+		if p := recover(); p != nil {
+			log.Print("recover")
+			err = &NotificationError{cause: p}
+		}
+	}()
 	if token == "" {
-		return &ShopTokenNotFoundError{shopID}
-	} else if err != nil {
-		return err
+		err = &ShopTokenNotFoundError{shopID}
 	}
 	err = s.APIWrapper.Send(token, message)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
 func (s *NotificationService) sendCustomer(customerID string, message string) error {
 	token, err := s.TokenRepository.GetCustomerToken(customerID)
+	defer func() {
+		if p := recover(); err != nil {
+			err = &NotificationError{cause: p}
+		}
+	}()
 	if token == "" {
 		return &CustomerTokenNotFoundError{customerID}
-	} else if err != nil {
-		return err
 	}
 	err = s.APIWrapper.Send(token, message)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
+// NewNotificationService constructs notification service
 func NewNotificationService(apiWrapper NotificationAPIWrapper, repository Repository) *NotificationService {
 	service := new(NotificationService)
 	service.APIWrapper = apiWrapper
